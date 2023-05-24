f690eb84781f2fa8c016262569f2e4586f06d9d0
osobuxs <lfexevega95@gmail.com>
2023-05-24 10:58:07 -0300

    tp 4

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  const [task, setTask] = useState("");
+  const [taskList, setTaskList] = useState([]);
+
+  const handleChange = (event) => {
+    setTask(event.target.value);
+  };
+
+  const handleKeyDown = (event) => {
+    if (event.key === "Enter") {
+      addTask();
+    }
+  };
+
+  const addTask = () => {
+    if (task.length >= 3 && task.length <= 400) {
+      setTaskList([...taskList, task]);
+      setTask("");
+    }
+  };
+
+  const deleteTask = (index) => {
+    const updatedTaskList = [...taskList];
+    updatedTaskList.splice(index, 1);
+    setTaskList(updatedTaskList);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <h1>Bienvenido</h1>
+      <div className="task-input">
+        <input
+          type="text"
+          placeholder="Ingresar una tarea"
+          value={task}
+          onChange={handleChange}
+          onKeyDown={handleKeyDown}
+        />
+        <button onClick={addTask}>Agregar</button>
+      </div>
+      <div className="task-list">
+        {taskList.map((item, index) => (
+          <div className="task-item" key={index}>
+            {item}
+            <span className="delete-icon" onClick={() => deleteTask(index)}>
+              X
+            </span>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
